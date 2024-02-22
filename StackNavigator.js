a8814649623fad20a7422cedf04cfd3325bc366d
umakant3525 <umakant3525@gmail.com>
2024-02-22 21:37:52 +0530

    done with ui also date with  and all ui

--- StackNavigator.js
+++ StackNavigator.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import PickUpScreen from './screens/PickUpScreen';
+import CartScreen from './screens/CartScreen';
 
 const StackNavigator = () => {
   const Stack = createNativeStackNavigator();
@@ -11,6 +12,7 @@ const StackNavigator = () => {
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen}  options={{headerShown:false}} />
         <Stack.Screen name="PickUp" component={PickUpScreen} />
+        <Stack.Screen name="Cart" component={CartScreen} options={{headerShown:false}}  />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- screens/PickUpScreen.jsx
+++ screens/PickUpScreen.jsx
@@ -1,11 +1,36 @@
-import { StyleSheet, Text, View, TextInput, ScrollView, Pressable } from 'react-native';
-import React, { useState } from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import HorizontalDatePicker from 'react-native-horizontal-date-picker';
-
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  ScrollView,
+  Pressable,
+  Button,
+  Alert,
+} from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { useNavigation } from '@react-navigation/native';
 const PickUpScreen = () => {
   const [selectedDate, setSelectedDate] = useState(null);
   const [selectedTime, setSelectedTime] = useState(null);
+  const [showDatePicker, setShowDatePicker] = useState(false);
+  const [delivery, setDelivery] = useState([]);
+  const navigation = useNavigation();
+
+  const handleDateSelected = (event, date) => {
+    setShowDatePicker(false);
+    if (date !== undefined) {
+      setSelectedDate(date);
+      console.log("Selected date:", date);
+    }
+  };
+
+  const handleTimeSelected = (time) => {
+    setSelectedTime(time);
+    console.log("Selected time:", time);
+  };
 
   const times = [
     {
@@ -21,7 +46,7 @@ const PickUpScreen = () => {
       time: "1:00 PM",
     },
     {
-      id: "3", // Corrected id
+      id: "3",
       time: "2:00 PM",
     },
     {
@@ -34,15 +59,56 @@ const PickUpScreen = () => {
     },
   ];
 
-  const handleDateSelected = (date) => {
-    setSelectedDate(date);
-    console.log("Selected date:", date);
-  };
+  const deliveryTime = [
+    {
+      id: "0",
+      name: "2-3 Days",
+    },
+    {
+      id: "1",
+      name: "3-4 Days",
+    },
+    {
+      id: "2",
+      name: "4-5 Days",
+    },
+    {
+      id: "3",
+      name: "5-6 Days",
+    },
+    {
+      id: "4",
+      name: "Tomorrow",
+    },
+  ];
 
-  const handleTimeSelected = (time) => {
-    setSelectedTime(time);
-    console.log("Selected time:", time);
+  const today = new Date();
+
+  const proceedToCart = () => {
+    if (!selectedDate || !selectedTime || !delivery) {
+      Alert.alert(
+        "Empty or invalid",
+        "Please select all the fields date time days ",
+        [
+          {
+            text: "Cancel",
+            onPress: () => console.log("Cancel Pressed"),
+            style: "cancel"
+          },
+          { text: "OK", onPress: () => console.log("OK Pressed") }
+        ],
+        { cancelable: false }
+      );
+    }
+    if (selectedDate && selectedTime && delivery) {
+      navigation.replace("Cart", {
+        pickUpDate: selectedDate,
+        selectedTime: selectedTime,
+        no_Of_days: delivery,
+      });
+    }
   };
+  
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
@@ -50,17 +116,34 @@ const PickUpScreen = () => {
         <Text style={styles.label}>Enter Address</Text>
         <TextInput style={styles.input} placeholder="Enter your address" />
 
-        <Text style={styles.label}>Pick Up Date</Text>
-        <HorizontalDatePicker
-          date={selectedDate}
-          onDateChange={handleDateSelected}
-          mode="day"
-          textColor="#222831"
-          fadeToColor="#ececec"
-          androidVariant="nativeAndroid"
-          style={{ marginBottom: 20 }}
+        <Button
+          title="Select Pick Up Date"
+          style={styles.label}
+          onPress={() => setShowDatePicker(true)}
         />
-        <Text style={styles.label}>Select Time</Text>
+
+        {showDatePicker && (
+          <DateTimePicker
+            value={selectedDate || today}
+            mode="date"
+            display="default"
+            onChange={handleDateSelected}
+            minimumDate={today} // Restrict selection to today and future dates
+            style={{ marginBottom: 20 }}
+            textColor="blue" // Custom text color
+            backgroundColor="lightgray" // Custom background color
+          />
+        )}
+
+        {selectedDate && (
+          <View style={styles.selectedDateTimeContainer}>
+            <Text style={styles.selectedDateTimeLabel}>
+              Selected Date: {selectedDate.toDateString()}{" "}
+            </Text>
+          </View>
+        )}
+
+        <Text style={styles.label}>Select Pick Time</Text>
         <ScrollView horizontal showsHorizontalScrollIndicator={false}>
           {times.map((item, index) => (
             <Pressable
@@ -71,20 +154,71 @@ const PickUpScreen = () => {
                 selectedTime === item.time && styles.selectedTimeButton,
               ]}
             >
-              <Text style={styles.timeButtonText}>{item.time}</Text>
+              <Text style={styles.buttonText}>{item.time}</Text>
             </Pressable>
           ))}
         </ScrollView>
 
-        {selectedDate && selectedTime && (
-          <View style={styles.selectedDateTimeContainer}>
-            <Text style={styles.selectedDateTimeLabel}>Selected Date and Time:</Text>
-            <Text style={styles.selectedDateTime}>
-              {selectedDate.toDateString()} {selectedTime}
-            </Text>
-          </View>
-        )}
+        <Text style={styles.label}>Delivery Date </Text>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          {deliveryTime.map((item, i) => (
+            <Pressable
+              style={[
+                styles.deliveryButton,
+                delivery.includes(item.name) && styles.selectedDeliveryButton,
+              ]}
+              onPress={() => setDelivery(item.name)}
+              key={i}
+            >
+              <Text style={styles.buttonText}>{item.name}</Text>
+            </Pressable>
+          ))}
+        </ScrollView>
       </View>
+
+      <Pressable
+        style={{
+          backgroundColor: "#088F8F",
+          padding: 10,
+          marginTop: 130,
+          marginHorizontal: 15,
+          borderRadius: 7,
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+        onPress={proceedToCart}
+      >
+        <View>
+          <Text style={{ fontSize: 17, fontWeight: "600", color: "white" }}>
+            4 items | RS 100
+          </Text>
+          <Text
+            style={{
+              fontSize: 10,
+              fontWeight: "400",
+              color: "white",
+              marginVertical: 6,
+            }}
+          >
+            Extra charges might apply
+          </Text>
+        </View>
+
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Text
+            style={{
+              fontSize: 15,
+              fontWeight: "600",
+              color: "white",
+              marginRight: 5,
+            }}
+          >
+            Proceed to Cart
+          </Text>
+          <Text style={{ fontSize: 20 }}>🛒</Text>
+        </View>
+      </Pressable>
     </SafeAreaView>
   );
 };
@@ -92,40 +226,40 @@ const PickUpScreen = () => {
 const styles = StyleSheet.create({
   label: {
     fontSize: 16,
-    fontWeight: '500',
+    fontWeight: "500",
     marginHorizontal: 10,
     marginTop: 10,
   },
   input: {
     padding: 10,
-    borderColor: 'gray',
+    borderColor: "gray",
     borderWidth: 0.7,
     borderRadius: 9,
     marginVertical: 10,
     paddingVertical: 40,
     marginHorizontal: 10,
   },
-  selectedItemTextStyle: {
-    color: 'white',
-    textAlign: 'center',
-    fontWeight: 'bold',
-  },
-  flatListContainerStyle: {
-    height: 50,
-    marginTop: 10,
-    marginHorizontal: 10,
-  },
   timeButton: {
     margin: 10,
     borderRadius: 7,
     padding: 15,
-    borderColor: 'gray',
+    borderColor: "gray",
     borderWidth: 0.7,
   },
   selectedTimeButton: {
-    borderColor: 'red',
+    borderColor: "red",
   },
-  timeButtonText: {
+  deliveryButton: {
+    margin: 10,
+    borderRadius: 7,
+    padding: 15,
+    borderColor: "gray",
+    borderWidth: 0.7,
+  },
+  selectedDeliveryButton: {
+    borderColor: "red",
+  },
+  buttonText: {
     fontSize: 16,
   },
   selectedDateTimeContainer: {
@@ -134,7 +268,7 @@ const styles = StyleSheet.create({
   },
   selectedDateTimeLabel: {
     fontSize: 16,
-    fontWeight: '500',
+    fontWeight: "500",
     marginBottom: 5,
   },
   selectedDateTime: {
